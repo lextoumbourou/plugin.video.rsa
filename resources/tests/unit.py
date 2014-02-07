3fa1a099e923be2acef3641350b7e8ff69570c06
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-07 13:50:35 +1100

    flake8

--- resources/tests/unit.py
+++ resources/tests/unit.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../../'))
 
 from resources.lib import rsa
 
+
 class UnitTests(unittest.TestCase):
     def test_scrape_video_list_returns_correct_values(self):
         contents = """
@@ -19,14 +20,12 @@ class UnitTests(unittest.TestCase):
         self.assertTrue(results[0]['title'] == 'Video Title')
         self.assertTrue(results[0]['thumbnail'] == 'thumb_url')
 
-
     def test_scrape_video_page_returns_youtube_id_if_available(self):
         contents = """
             <meta name="youtube_url" content="XBmJay_qdNc" />
         """
         youtube_id = rsa.scrape_video_page(contents)
         self.assertTrue(youtube_id == 'XBmJay_qdNc')
-
 
     def test_scrape_video_page_returns_youtube_id_when_url_in_meta_tag(self):
         contents = """
@@ -35,9 +34,9 @@ class UnitTests(unittest.TestCase):
         youtube_id = rsa.scrape_video_page(contents)
         self.assertTrue(youtube_id == 'nh-hW0uG_zs')
 
-    def test_scrape_video_page_returns_youtube_id_when_id_not_in_meta_tag(self):
+    def test_scrape_video_page_returns_youtube_id_when_not_in_meta_tag(self):
         contents = """
-            <meta name="no_what_i_want" content="" /> 
+            <meta name="no_what_i_want" content="" />
             <iframe src="the_wrong_one"></iframe>
             <div id="new_div_1526142">
                 <iframe width="560" height="315"
